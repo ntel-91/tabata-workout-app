546a2c6d3fe1fd1ca6c95409833773d1c9e15bb6
ntel-91 <ntelenson@gmail.com>
2020-04-29 18:29:15 -0400

    added timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import styled from 'styled-components'
 
-function Timer () {
+const Timer = () => {
+    const [ time, setTime ] = useState(60)
+    const [ isActive, setIsActive ] = useState(false)
+    // const timeRef = useRef(time)
+
+    useEffect(() => {
+        let interval = null
+        if (isActive) {
+            interval = setInterval(() => {
+                setTime(time => time - 1)
+            }, 1000)
+        }
+        return () => clearInterval(interval)
+    })
+
+    const toggle = () => {
+        setIsActive(!isActive)
+    }
+
     return (
-        <h2>Test</h2>
+        <Time>
+            {time}
+
+            <button onClick={toggle}>{isActive ? 'Pause' : 'Start'}</button>
+        </Time>
     )
 }
 
 export default Timer
+
+const Time = styled.div`
+    font-size: 60px;
+`
